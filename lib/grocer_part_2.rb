4bce76a4c222e0b80d1da97ba012d8e8903cc8c0
codified-likeness-utility <mr.daviesian@gmail.com>
2021-02-19 15:03:30 -0500

    commit tree 5e3015b482d993bca3e14f35ac7301c4944b6ac6

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,12 +1,23 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  cart_counter = 0
+    consolidate_cart(cart).map do |grocery_items|
+    new_cart = grocery_items
+    coupon_applied_cart = new_cart
+      if new_cart[:item] = coupons[0][:item]
+        coupon_applied_cart << {:item => new_cart[:item] + " W/COUPON", :price => coupons[0][:cost]/coupons[0][:num], :clearance => true, :count => 2}
+      end 
+      #binding.pry
+  end
+  coupon_applied_cart
 end
 
 def apply_clearance(cart)
+
+
+  # returns a new Array where every unique item in the original is present 
+  # but with its price reduced by 20% if its :clearance value is true
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
